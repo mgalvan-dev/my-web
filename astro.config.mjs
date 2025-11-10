2245ed754e6e1d688c4bd7539b742bb089b8c7a7
Marco Galvan <elmacro11@gmail.com>
2025-11-10 19:10:33 -0300

    feat: improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mgalvan.dev",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        locales: { es: "es", en: "en" },
+      },
+      changefreq: "monthly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
+});
